818e30ae584021c422ea3c051b18a28c00024d5b
Paul T. Baker <paultbaker@gmail.com>
2014-05-30 13:18:23 -0400

    as hasting module for logL, logP calculations; add prior draw to proposal

--- mmi_lib/prop.py
+++ mmi_lib/prop.py
@@ -6,17 +6,20 @@
 # licence: GNU GPLv3 <http://www.gnu.org/licenses/gpl.txt>
 
 import numpy as np
-import random as rand
 
 
 def proposal(Ga, F):
     """ propose a new state for MCMC
     
     return new state (Gb) and proposal density (dlogQ) for Hastings Ratio
+    dlogQ = logQb - logQa, same as dP and dL: subtracted in HR
     """
 #   TODO: implement suite of proposals... 
 #         prior draw, ???
-    if rand.random()<0.5:
+    test = np.random.random()
+    if test<0.05:
+        Gb, dlogQ = prior(Ga)
+    elif test<0.525:
         Gb, dlogQ = step(Ga)
     else:
         Gb, dlogQ = fisher(Ga, F)
@@ -24,19 +27,24 @@ def proposal(Ga, F):
     return (Gb, dlogQ)
 
 def step(Ga):
-    Gb = Ga.copy()
-    # random direction, gaussian 1-sigma jump
-    for i in range(Ga.shape[0]):
-        for j in range(Ga.shape[1]):
-            dG = rand.gauss(0.0,0.1)/float(Ga.size)
-            Gb[i,j] = Ga[i,j] + dG
+    """random direction, 1-sigma gaussian step"""
+    dG = np.random.normal(size=Ga.shape)/float(Ga.size)
+    Gb = Ga + dG
     dlogQ = 0.  # jump is symmetric: Qa == Qb => dQ=0.
     return (Gb, dlogQ)
 
 def fisher(Ga, F):
-    i = rand.randrange(Ga. size)
-    dG = 1.0/np.sqrt(F.val[i]) * rand.gauss(0.0,1.0) * F.vec[i,:,:]
+    """gaussian step along random eigenvector of F""" 
+    i = np.random.randint(Ga.size) # choose eigenvector
+    dG = 1.0/np.sqrt(F.val[i]) * np.random.normal() * F.vec[i,:,:]
     Gb = Ga + dG
     dlogQ = 0.  # fisher jump is symmetric
+    return (Gb, dlogQ)
+
+def prior(Ga):
+    """prior draw, gaussian prior"""
+    #TODO: sigma=5 hardcoded
+    Gb = np.random.normal(scale=5.0,size=Ga.shape)
+    dlogQ = mmi.hasting.log_P(Gb) - mmi.hasting.log_P(Ga)
     return (Gb, dlogQ)
 
